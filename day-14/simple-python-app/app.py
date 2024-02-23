61b8ce06d75510cbcfcb45e9421208006afe0794
venkateswara reddy <venkatesh.reddy679@gmail.com>
2024-02-22 19:56:33 -0500

    .

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
-    return 'Hello, world! venky'
-=======
-    return 'Hello, world! venkateswara reddy!'
->>>>>>> 45c75e3bb89541d56f3494b89614da23bcdd7a3f
-
+    return 'Hello, world! venky guduru'
 
 if __name__ == '__main__':
     app.run()
