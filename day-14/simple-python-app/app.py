66133f56725ed679ecf758a09c2baa5b760a7482
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-02-22 20:04:18 -0500

    Update app.py
    modified text

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! venky guduru'
+    return 'Hello, world! hosbcjkdlbcjkdbcdvbkhbdlcbadljcbdh cld ched'
 
 if __name__ == '__main__':
     app.run()
