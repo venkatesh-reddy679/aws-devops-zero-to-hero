cc7875b843511e5759e21cd591220d10d9eb630f
venkatesh-reddy679 <60383183+venkatesh-reddy679@users.noreply.github.com>
2024-09-27 16:07:01 -0400

    Update container_start.sh

--- scripts/container_start.sh
+++ scripts/container_start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "this script is to run the container by pulling the docker image from AWS ECR"
-
+aws ecr get-login-password | docker login -u AWS --password-stdin 084828606525.dkr.ecr.us-east-2.amazonaws.com/
 docker pull 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:8
 docker run -dt -p 5000:5000 --name pythonapp 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:8
 docker ps
