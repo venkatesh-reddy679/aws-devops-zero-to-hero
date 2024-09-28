73129a917bb7f66464e9d1b44299029692e6c3b8
venkatesh <venkatesh.reddy679@gmail.com>
2024-09-28 03:49:59 +0000

    updated container_star.sh script with build number 22

--- scripts/container_start.sh
+++ scripts/container_start.sh
@@ -2,8 +2,8 @@
 
 echo "this script is to run the container by pulling the docker image from AWS ECR"
 aws ecr get-login-password | docker login -u AWS --password-stdin 084828606525.dkr.ecr.us-east-2.amazonaws.com/
-docker pull 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:21
-docker run -dt -p 5000:5000 --name pythonapp 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:21
+docker pull 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:22
+docker run -dt -p 5000:5000 --name pythonapp 084828606525.dkr.ecr.us-east-2.amazonaws.com/test-image:22
 docker ps
 
 echo "container is created and port 5000 is exposed"
